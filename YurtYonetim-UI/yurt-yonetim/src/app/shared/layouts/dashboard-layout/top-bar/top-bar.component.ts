469a572bca2150d0aff17bfc4cb5b9db2e523168
engin can <engin_can1528@hotmail.com>
2022-07-02 22:24:32 +0300

    çıkış işlemi yapıldı

--- YurtYonetim-UI/yurt-yonetim/src/app/shared/layouts/dashboard-layout/top-bar/top-bar.component.ts
+++ YurtYonetim-UI/yurt-yonetim/src/app/shared/layouts/dashboard-layout/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { LoginUser } from 'src/app/shared/core/dto/general.dto';
 
 @Component({
   selector: 'in-top-bar',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
-  constructor() { }
+  loginUser = new LoginUser();
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
+    this.authService.getLoginUser().subscribe((res)=>{
+      this.loginUser = res;
+    });
+  }
+
+  signOut(){
+    this.authService.logoutWithRefresh()
   }
 
 }
